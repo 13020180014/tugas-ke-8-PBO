1fc17cd7686494221d1a0108effe6286eac1129c
13020180014 <61999864+13020180014@users.noreply.github.com>
2020-05-05 18:02:10 +0700

    Add files via upload

---
+++ Tugas 8/Test.java
@@ -0,0 +1,35 @@
+/* Nama : Nabilatul Rahma
+   Stambuk: 13020180014
+   tgl pengerjaan : 3 mei 2020
+   waktu : 20:45 */
+public class Test 
+
+
+  { public static int counter=0;
+
+
+    public Test()
+
+
+    { counter +=1; }
+
+
+
+
+
+     public int getCounter()
+
+
+     {  return counter;  }     
+
+
+
+
+
+     public void addCounter()
+
+
+     {  counter +=1;  }     
+
+
+  }
